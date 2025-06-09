cd842614fc5cbd3af2bd70294896440ed8d31c22
Santiago <concursos.utn.frt@gmail.com>
2025-06-09 20:33:52 -0300

    agregamos el maquetado del componente información

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Informacion from './components/Informacion'
+
 function App() {
-  return <h1>Hola Mundo</h1>;
+  //Aqui va la logica del componente
+  return (
+  //Aqui puedo agregar algo de logica
+   <main className="container my-5">
+    {/*Aqui se agrega el maquetado html*/}
+    <h1>Hola Mundo</h1>;
+   <Informacion></Informacion>
+     </main>
+     );
 }
+ 
 
 export default App
