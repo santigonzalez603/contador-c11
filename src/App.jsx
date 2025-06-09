e7551aaf2895b537c329d7724bbf3b7e541bca65
Santiago <concursos.utn.frt@gmail.com>
2025-06-09 20:59:02 -0300

    enviamos un props desde app a informacion

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Informacion from './components/Informacion'
 
 function App() {
   //Aqui va la logica del componente
+  const comision = 'c11'
   return (
   //Aqui puedo agregar algo de logica
    <main className="container my-5">
     {/*Aqui se agrega el maquetado html*/}
     <h1>Hola Mundo</h1>;
-   <Informacion></Informacion>
+   <Informacion comisionProps={comision} anio={2025}/>
      </main>
      );
 }
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,9 +1,12 @@
-const Informacion = () => {
+const Informacion = (props) => {
     return (
         <section>
             <h2>Primer proyecto con react</h2>
             <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario. <br/>Se destaca por su enfoque en la creación de aplicaciones web de una sola página SPA y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un <b>DOM virtual</b>.</p>
             <p>React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
+            <article className="bg-primary-subtle p-3 rounded-2">
+                <p>Practica del contador de la <strong>comisicion {props.comisionProps}</strong></p>
+            </article>
         </section>
     );
 };
