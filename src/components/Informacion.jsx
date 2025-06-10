03e69c488d760a653fb60d36e230781e0d13a6e3
Santiago <concursos.utn.frt@gmail.com>
2025-06-09 21:11:05 -0300

    probamos otra sintaxtis para usar un props

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,11 +1,11 @@
-const Informacion = (props) => {
+const Informacion = ({anio, comisionProps}) => {
     return (
         <section>
             <h2>Primer proyecto con react</h2>
             <p>React es una biblioteca de JavaScript desarrollada por Facebook para construir interfaces de usuario. <br/>Se destaca por su enfoque en la creación de aplicaciones web de una sola página SPA y su capacidad para manejar la interfaz de manera eficiente mediante el uso de un <b>DOM virtual</b>.</p>
             <p>React permite a los desarrolladores construir interfaces dinámicas y reactivas de forma modular, lo que facilita el desarrollo y el mantenimiento de aplicaciones a gran escala.</p>
             <article className="bg-primary-subtle p-3 rounded-2">
-                <p>Practica del contador de la <strong>comisicion {props.comisionProps}</strong></p>
+                <p>Practica del contador de la <strong>comisicion {comisionProps}</strong> - {anio}</p>
             </article>
         </section>
     );
