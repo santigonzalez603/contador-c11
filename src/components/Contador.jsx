72f1d1088f2140339488c4bfe836e7c49f6eb79b
Santiago <concursos.utn.frt@gmail.com>
2025-06-09 21:34:02 -0300

    agregamos la logica de sumar del boton del contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 const Contador = () => {
+    //Crear un estado
+    const [numero, setNumero] = useState(1)
+    //let numero = 1
+    //numero = 345
+    //setNumero(345)
+    const sumar = ()=>{
+        setNumero(numero + 1)
+        console.log(numero)
+    }
     return (
         <section className="text-center mt-5">
             <h2>Contador</h2>
-            <h3>N°: 1</h3>
-            <button className="btn btn-success">+</button>
+            <h3>N°: {numero}</h3>
+            <button className="btn btn-primary me-3" onClick={sumar}>+</button>
             <button className="btn btn-danger">-</button>
         </section>
     );
